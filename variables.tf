57b3a6088dff513fe3af94e91fa2df5030e43fd6
Dibbs <29274865+tristandibbens@users.noreply.github.com>
2023-01-05 15:53:28 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,3 +28,44 @@ variable "dns_servers" {
   type = list(string)
   default = []
 }
+
+# expected values - certificate-authentication to use certificate-based authentication, directory-service-authentication to use Active Directory authentication, or federated-authentication
+variable "client_auth" {
+  description = "The expected type of the VPN authentication"
+  default = "certificate-authentication"
+}
+
+variable "saml_provider_arn" {
+  description = "The arn of the pre configured SAML app"
+  type = string
+  default = ""
+}
+
+variable "vpc_id" {
+  description = "The VPC id containing security groups, must be included if passing security group ids"
+  type = string
+}
+
+variable "security_group_ids" {
+  description = "The security group ids required, must pass vpc ID as well"
+  type = list(string)
+  default = []
+}
+
+variable "cloudwatch_enabled" {
+  description = "Enables logging"
+  type = bool
+  default = true
+}
+
+variable "cloudwatch_log_group" {
+  description = "The name of the cloudwatch log group."
+  type        = string
+  default = "vpn_endpoint_cloudwatch_log_group"
+}
+
+variable "cloudwatch_log_stream" {
+  description = "The name of the cloudwatch log stream."
+  type        = string
+  default = "vpn_endpoint_cloudwatch_log_stream"
+}
