0ca29e4cd57dcf91dfdabe5811a0ce21d2915f8f
Dibbs <29274865+tristandibbens@users.noreply.github.com>
2025-04-11 20:49:51 +0100

    Update client-vpn.tf
    certificate refresh, uncommenting out the null resources to generate the ovpn cert

--- client-vpn.tf
+++ client-vpn.tf
@@ -75,31 +75,31 @@ resource "aws_ec2_client_vpn_network_association" "client-vpn-network-associatio
 }
 
 # Ran this manually
-# resource "null_resource" "authorize-client-vpn-ingress" {
-#  provisioner "local-exec" {
-#    when = create
-#    command = "aws ec2 authorize-client-vpn-ingress --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --target-network-cidr 0.0.0.0/0 --authorize-all-groups"
-#  }
-#
-#  depends_on = [
-#    aws_ec2_client_vpn_endpoint.client-vpn-endpoint,
-#    aws_ec2_client_vpn_network_association.client-vpn-network-association
-#  ]
-#}
+resource "null_resource" "authorize-client-vpn-ingress" {
+  provisioner "local-exec" {
+    when = create
+    command = "aws ec2 authorize-client-vpn-ingress --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --target-network-cidr 0.0.0.0/0 --authorize-all-groups"
+  }
 
-#resource "null_resource" "export-client-config" {
-#  provisioner "local-exec" {
-#    when = create
-#    command = "aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --output text > ./client-config.ovpn"
-#  }
-#
-#  depends_on = [
-#    aws_ec2_client_vpn_endpoint.client-vpn-endpoint,
-#    #null_resource.authorize-client-vpn-ingress,
-#    #null_resource.create-client-vpn-route,
-#    aws_ec2_client_vpn_network_association.client-vpn-network-association,
-#  ]
-#}
+  depends_on = [
+    aws_ec2_client_vpn_endpoint.client-vpn-endpoint,
+    aws_ec2_client_vpn_network_association.client-vpn-network-association
+  ]
+}
+
+resource "null_resource" "export-client-config" {
+  provisioner "local-exec" {
+    when = create
+    command = "aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --output text > ./client-config.ovpn"
+  }
+
+  depends_on = [
+    aws_ec2_client_vpn_endpoint.client-vpn-endpoint,
+    #null_resource.authorize-client-vpn-ingress,
+    #null_resource.create-client-vpn-route,
+    aws_ec2_client_vpn_network_association.client-vpn-network-association,
+  ]
+}
 
 resource "null_resource" "append-client-config-certs" {
   provisioner "local-exec" {
