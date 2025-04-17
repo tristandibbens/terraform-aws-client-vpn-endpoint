73c28d3af4f94e21ccf5e90689b5347fec97a8c9
Dibbs <29274865+tristandibbens@users.noreply.github.com>
2025-04-17 11:59:03 +0100

    Update outputs.tf
    added the client_cidr_block output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "client_vpn_endpoint_id" {
   value = aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id
 }
 
+output "client_vpn_cidrs" {
+  value = aws_ec2_client_vpn_endpoint.client-vpn-endpoint.client_cidr_block
+}
